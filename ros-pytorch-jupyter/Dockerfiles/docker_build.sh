ec43316fdeaeb6ccc80025d74293dcd12cd01f59
coolcat647 <b10302017@mail.ntust.edu.tw>
2019-08-18 18:53:34 +0800

    add ros-pytorch

---
+++ ros-pytorch-jupyter/Dockerfiles/docker_build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+COLOR_RED='\033[0;31m'
+COLOR_YELLOW='\033[0;33m'
+COLOR_NC='\033[0m'
+
+if [ $# -gt 0 ]; then
+    if [ "$1" == "cuda8" ]; then
+        docker build -t coolcat647/ros-pytorch:cuda8.0 -f Dockerfile-cuda8 .
+    elif [ "$1" == "cuda8.0" ]; then
+        docker build -t coolcat647/ros-pytorch:cuda8.0 -f Dockerfile-cuda8 .
+    elif [ "$1" == "cuda10" ]; then
+        docker build -t coolcat647/ros-pytorch:cuda10.0 -f Dockerfile-cuda10 .
+    elif [ "$1" == "cuda10.0" ]; then
+        docker build -t coolcat647/ros-pytorch:cuda10.0 -f Dockerfile-cuda10 .
+    else
+        echo -e "Please specify which cuda version your GPU support."
+        echo -e "${COLOR_RED}Usage: source docker_build.sh [cuda8 | cuda10]${COLOR_NC}"
+    fi
+else
+    echo -e "${COLOR_RED}Usage: source docker_build.sh [cuda8 | cuda10]${COLOR_NC}"
+fi
