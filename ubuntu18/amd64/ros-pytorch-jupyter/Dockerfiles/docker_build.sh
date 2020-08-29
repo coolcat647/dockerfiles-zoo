1c4d19979eb35b063d5f4b213a6fd5fbe77ab6fc
coolcat647 <b10302017@mail.ntust.edu.tw>
2020-08-29 23:02:29 +0800

    maintain ubuntu18-ros-pytorch docker image

--- ubuntu18/amd64/ros-pytorch-jupyter/Dockerfiles/docker_build.sh
+++ ubuntu18/amd64/ros-pytorch-jupyter/Dockerfiles/docker_build.sh
@@ -3,15 +3,4 @@ COLOR_RED='\033[0;31m'
 COLOR_YELLOW='\033[0;33m'
 COLOR_NC='\033[0m'
 
-if [ $# -gt 0 ]; then
-    if [[ "$1" == "cuda9" || "$1" == "cuda9.0" ]] ; then
-        docker build -t coolcat647/ubuntu18-ros-pytorch:cuda9.0 --build-arg user_id=$(id -u) -f Dockerfile-cuda9 . 
-    elif [[ "$1" == "cuda10" || "$1" == "cuda10.0" ]] ; then
-        docker build -t coolcat647/ubuntu18-ros-pytorch:cuda10.0 --build-arg user_id=$(id -u) -f Dockerfile-cuda10 .
-    else
-        echo -e "Please specify which cuda version your GPU support. If you do not have any GPU, please feel free to choose one."
-        echo -e "${COLOR_RED}Usage: source docker_build.sh [cuda9 | cuda10]${COLOR_NC}"
-    fi
-else
-    echo -e "${COLOR_RED}Usage: source docker_build.sh [cuda9 | cuda10]${COLOR_NC}"
-fi
+docker build -t coolcat647/ubuntu18-ros-pytorch:cuda10.0 --build-arg user_id=$(id -u) -f Dockerfile-cuda10 .
