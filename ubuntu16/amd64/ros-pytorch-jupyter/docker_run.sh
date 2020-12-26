45ca81dbc3b418bb262dca762716ff03de9aaef1
coolcat647 <b10302017@mail.ntust.edu.tw>
2020-12-26 16:57:19 +0800

    maintain u16 docker

--- ubuntu16/amd64/ros-pytorch-jupyter/docker_run.sh
+++ ubuntu16/amd64/ros-pytorch-jupyter/docker_run.sh
@@ -5,7 +5,27 @@ COLOR_RED='\033[0;31m'
 COLOR_YELLOW='\033[0;33m'
 COLOR_NC='\033[0m'
 
+
+#
+# Check the command 'nvidia-docker' existed or not
+#
+ret_code="$(command -v nvidia-docker)"
+if [ -z "$ret_code" ]; then
+    if [ -z "$(command -v nvidia-container-cli)" ]; then
+        NVIDIA_SUPPORT_OPTION="--gpus all "
+    else
+        NVIDIA_SUPPORT_OPTION=""
+    fi
+    DOCKER_CMD="docker"
+else
+    DOCKER_CMD="nvidia-docker"
+    NVIDIA_SUPPORT_OPTION=""
+fi
+
+
+#
 # Specify cuda version
+#
 if [ $# -gt 0 ]; then
    if [[ "$1" == "cuda9" || "$1" == "cuda9.0" ]] ; then
         docker_tag="cuda9.0"
@@ -19,35 +39,39 @@ else
     echo -e "${COLOR_RED}Usage: source docker_run.sh [cuda9 | cuda10]${COLOR_NC}"
 fi
 
+#
 # Find current directory and transfer it to container directory for Docker
-jupyter_port="8888"
+#
 current_dir="$(pwd)"
 host_dir="${HOME}/"
-container_dir="/root/"
+container_dir="/home/developer/"
 goal_dir=${current_dir//$host_dir/$container_dir}
 #echo "goal_dir: \"${goal_dir}\""
 
-# export env
-export JUPYTER_PORT="${jupyter_port}"
 
-# Check the command 'nvidia-docker' is existing or not
-ret_code="$(command -v nvidia-docker)"
-if [ -z "$ret_code" ]
-then
-    printf "${COLOR_YELLOW}\"nvidia-docker\" is not found, so substitute docker. ${COLOR_NC}\n"
-
-    docker run -it --rm --net=host \
-                        -v ${current_dir}:${goal_dir} \
-                        -w "${goal_dir}" \
-                        -e JUPYTER_PORT="${jupyter_port}" \
-                        --name ros-pytorch \
-                        coolcat647/ros-pytorch:${docker_tag}
+#
+# Execute command
+#
+if [ $# -gt 0 ]; then
+    if [ "$1" == "same" ]; then
+        docker exec -it u16-ros-pytorch bash
+    else
+        ${DOCKER_CMD} run ${NVIDIA_SUPPORT_OPTION} --name u16-ros-pytorch --rm -it --net=host --privileged -v /dev:/dev \
+            -e DISPLAY=$DISPLAY \
+            -v /etc/localtime:/etc/localtime:ro -v /var/run/docker.sock:/var/run/docker.sock \
+            -v ${current_dir}:${goal_dir} \
+            -v /tmp/.X11-unix/:/tmp/.X11-unix:rw \
+            -w ${goal_dir} \
+            --device=/dev/dri:/dev/dri \
+            --device=/dev/nvhost-ctrl \
+            --device=/dev/nvhost-ctrl-gpu \
+            --device=/dev/nvhost-prof-gpu \
+            --device=/dev/nvmap \
+            --device=/dev/nvhost-gpu \
+            --device=/dev/nvhost-as-gpu \
+            -v /dev/bus/usb:/dev/bus/usb \
+            coolcat647/ros-pytorch:${docker_tag}
+    fi
 else
-    printf "Run \"nvidia-docker\"\n"
-    nvidia-docker run -it --rm --net=host \
-                               -v ${current_dir}:${goal_dir} \
-                               -w ${goal_dir} \
-                               -e JUPYTER_PORT="${jupyter_port}" \
-                               --name ros-pytorch \
-                               coolcat647/ros-pytorch:${docker_tag}
+    echo "please provide docker tag name."
 fi
